623c9115e4d518888150f93ae7664cfb97d5bcd6
Sebastian D. Eastham <sdeastham@googlemail.com>
2024-05-26 19:51:20 +0100

    Working HUD with configuration check
    The configuration file can now be selected (a default one has been included) and a simulation can only begin once its validity is checked.

---
+++ Hud.cs
@@ -0,0 +1,60 @@
+using Godot;
+using System;
+using DroxtalWolf;
+
+public partial class Hud : CanvasLayer
+{
+	[Signal]
+	public delegate void StartSimulationEventHandler(string configPath);
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Check if the default file is producing a valid config..
+		CheckConfig();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void OnStartButtonPressed()
+	{
+		GetNode<Button>("BeginSimulationButton").Hide();
+		GetNode<Button>("ConfigSelectButton").Hide();
+		GetNode<Label>("ConfigPathLabel").Hide();
+		string configPath = GetNode<Label>("ConfigPathLabel").Text;
+		EmitSignal(SignalName.StartSimulation,configPath);
+	}
+
+	private void OnChooseFileButtonPressed()
+	{
+		GetNode<FileDialog>("ConfigFileDialog").Show();
+	}
+
+	private void OnFileSelection(string filePath)
+	{
+		GetNode<Label>("ConfigPathLabel").Text = filePath;
+		// DWCore needs the real path
+		CheckConfig();
+	}
+
+	private void CheckConfig()
+	{
+		string filePath = GetNode<Label>("ConfigPathLabel").Text;
+		string globalPath = ProjectSettings.GlobalizePath(filePath);
+		// Attempt to read the configuration
+		try
+		{
+			RunOptions runOptions = RunOptions.ReadConfig(globalPath);
+			GetNode<Button>("BeginSimulationButton").Disabled = false;
+		}
+		catch (Exception e)
+		{
+			GD.Print(e);
+			GetNode<Button>("BeginSimulationButton").Disabled = true;
+			throw;
+		}
+	}
+}
